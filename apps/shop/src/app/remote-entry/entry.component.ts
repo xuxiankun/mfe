228b934371ee6146f053298125a068164ee72a7a
Jimmy Xu <jxu@billtrust.com>
2022-09-03 19:54:04 -0600

    init

---
+++ apps/shop/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'acme-shop-entry',
+  template: `<acme-nx-welcome></acme-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
